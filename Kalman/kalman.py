754f6c4dfa5eb83fd26541bd8af6404150b3890f
Silvia Ferrer <62883007+silviaferrer@users.noreply.github.com>
2022-11-29 12:23:02 +0100

    Changes in kalman.py

--- Kalman/kalman.py
+++ Kalman/kalman.py
@@ -1,13 +1,13 @@
 from filterpy.kalman import KalmanFilter
 from filterpy.common import Q_discrete_white_noise
-import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
 class Kalman: 
 
- def __init__(self, fs, alpha0, omega0):
+ def __init__(self, fs, alpha0, omega0, t):
   self.fs = fs
+  self.t = t
   self.state = [alpha0, omega0]
   k = KalmanFilter (dim_x=2, dim_z=2)                       #initialization
   k.x = np.array([alpha0, omega0])                          #intial state (position and velocity)
@@ -28,11 +28,11 @@ class Kalman:
     alpha = alpha + omega*t
     return [alpha, omega]
 
- def filtrar(self, measure, t):
+ def filtrar(self, measure):
    self.k.predict()
-   self.k.update(self.angular_vector(self.k.x[0], measure, t))
+   self.k.update(self.angular_vector(self.k.x[0], measure, self.t))
    self.state = self.k.x
-   return Kalman(self.fs, self.state, self.k )
+   return self.state, self.k
 
 def get_position(self):
     return self.state[0]
@@ -40,36 +40,9 @@ def get_position(self):
 def get_velocity(self):
     return self.state[1]
 
-
 def lectura(fichero):
     f = open(fichero, 'r')
     datos = []
     for linea in f:
      datos.append(float(linea.strip('\n')))
     return datos
-
-
-# Lectura de fichero de prueba
-fichero = 'datos3.txt'
-datos = lectura(fichero)
-fs = 100
-t = 1/fs
-longitud = len(datos)
-tiempo = np.arange(0, (longitud)/100, t)
-output = []
-
-kal = Kalman(fs, 0, 0)
-
-for i in range(longitud):
-  kal.filtrar(datos[i], t)
-  output.append(kal.x[1])
-
-fig = plt.figure()
-# plt.axis([0, 10, 0, 1])
-plt.title('Data from gyroscope', fontweight='bold')
-plt.xlabel('Time')
-plt.ylabel('Velocity (rad/s)')
-plt.plot(tiempo, datos, 'k',label='noisy measurements')
-plt.plot(tiempo, output, 'r',label='Filter signal')
-plt.show()
-
