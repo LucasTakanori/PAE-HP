ca151f5e3ffc2b3cf207cc6dcb25372ec8b87be8
alexsoler9 <114919581+alexsoler9@users.noreply.github.com>
2023-01-23 14:15:06 +0100

    Add files via upload

--- Gyro_Encoders/Arduino_encoders_gyro/data_log.py
+++ Gyro_Encoders/Arduino_encoders_gyro/data_log.py
@@ -3,9 +3,9 @@ import serial
 
 SERIAL_PORT = 'COM3'
 SERIAL_BAUD = 57600
-OUT_FILE = 'angle_encoders_gyro_getMotion_1.csv'
+OUT_FILE = 'final_angle_encoders_gyro_getMotion_10.csv'
 FS = 100
-MEAS_DUR_SEC = 120
+MEAS_DUR_SEC = 3600
 
 TS = 1.0 / FS
 N_SAMPLES = int(MEAS_DUR_SEC * FS)
@@ -17,9 +17,7 @@ with open(OUT_FILE, newline='', mode='w') as csvfile:
     csvwriter = csv.writer(csvfile, delimiter=',')
 
     for _ in range(N_SAMPLES):
-
         dataList= ser.readline().decode('utf-8').strip('\r\n').split(',')
-
         csvwriter.writerow(dataList)
 
 ser.close()
